6544f03dd832f3faac8f334e498afdb32076a38d
Personwhoexists <82169403+personwhoexist@users.noreply.github.com>
2022-05-17 14:03:06 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@ function ok() {
         setTimeout(ok, 0.001)
     }
     if (ok1 == 211) {
-        document.write("fineding");
+        document.write("fin");
         ok11();
     }
     if (ok1 == pos) {
