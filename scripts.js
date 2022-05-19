946d3d86a254d5b6e67177d6299c1343bc95c10f
Personwhoexists <82169403+personwhoexist@users.noreply.github.com>
2022-05-19 09:41:01 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ function ok() {
     }
 }
 function ok11() {
-    document.write("ok");
+    document.write("no");
     document.addEventListener('keydown', function okk(e) {
         if (e.code == 'KeyD') {
             if (test == true) {
